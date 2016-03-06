19916e9480c71a58e3c26a3bd62998a525ad77df
Kuba Orlik <kontakt@jakub-orlik.pl>
2016-03-06 20:46:38 +0100

    Update mongodb version to enable full-text search

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var Promise = require("bluebird");
 var Sealious = require("sealious");
-var Mongodb = require("mongodb");
+var MongoClient = require('mongodb').MongoClient
+var assert = require("assert");
 var DbsCommonPart = require('sealious-datastore-dbs-common-part');
 
 var private = {db: null};
@@ -18,15 +19,33 @@ Sealious.ConfigManager.set_default_config(
 );
 
 DatastoreMongo.start = function(){
+	var self = this;
 	var config = Sealious.ConfigManager.get_config("datastore_mongo");
+
+	var url = `mongodb://${config.host}:${config.port}/${config.db_name}`;
+	return new Promise(function(resolve, reject){
+		MongoClient.connect(url, function(err, db) {
+			if (db === null){
+				reject("MongoDB was not found, please make sure it's installed. Check https://docs.mongodb.org/manual/tutorial/ for more info.");
+				return;
+			} else {
+				private.db = db;				
+				self.post_start().then(function(){
+					resolve();
+				})
+			}
+		});		
+	})
+
+
 	var mongo_client = new Mongodb.MongoClient(new Mongodb.Server(config.host, config.port));
 	return new Promise(function(resolve, reject){
-		mongo_client.open(function(err, mongoClient){
+		mongo_client.connect(function(err, mongoClient){
 			if (mongoClient === null)
 				reject("MongoDB was not found, please make sure it's installed. Check https://docs.mongodb.org/manual/tutorial/ for more info.");
 			else {
 				private.db = mongoClient.db(config.db_name);
-				resolve();
+				
 			}
 		});
 	});	
