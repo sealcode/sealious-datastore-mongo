7c1bf750ab5db2d8311f1c03781314c698bf583a
Kuba Orlik <kontakt@jakub-orlik.pl>
2016-11-01 21:28:01 +0100

    Reflect changes in sealcode/sealious#06221fe752097f2d2

--- index.js
+++ index.js
@@ -1,40 +1,28 @@
+"use strict";
 var Promise = require("bluebird");
-var Sealious = require("sealious");
 var MongoClient = require('mongodb').MongoClient;
-var assert = require("assert");
 var DbsCommonPart = require('sealious-datastore-dbs-common-part');
 
-var private = {db: null};
+module.exports = function(App){
+	const priv = {db: null};
 
-var DatastoreMongo  = new Sealious.ChipTypes.Datastore("mongo");
+	var DatastoreMongo  = App.createDatastore({name:"mongo"});
 
-Sealious.ConfigManager.set_default_config(
-	"datastore_mongo", 
-	{
-		embedded: false,
-		host: 'localhost',
-		port: 27017,
-		db_name: 'sealious'
-	}
-);
+	DatastoreMongo.start = function(){
+		var self = this;
+		var config = App.ConfigManager.get_config().datastore_mongo;
 
-DatastoreMongo.start = function(){
-	var self = this;
-	var config = Sealious.ConfigManager.get_config("datastore_mongo");
+		var url = `mongodb://${config.host}:${config.port}/${config.db_name}`;
+		return Promise.promisify(MongoClient.connect)(url)
+			.then(function(db){
+				if (db === null){
+					return Promise.reject("MongoDB was not found, please make sure it's installed. Check https://docs.mongodb.org/manual/tutorial/ for more info.");
+				} else {
+					priv.db = db;
+					return self.post_start();
+				}
+			});
+	};
 
-	var url = `mongodb://${config.host}:${config.port}/${config.db_name}`;
-	return Promise.promisify(MongoClient.connect)(url)
-	.then(function(db){
-		if (db === null){
-			return Promise.reject("MongoDB was not found, please make sure it's installed. Check https://docs.mongodb.org/manual/tutorial/ for more info.");
-		} else {
-			private.db = db;
-			return self.post_start();
-		}
-	});
+	DbsCommonPart(App, DatastoreMongo, priv);
 };
-
-
-DatastoreMongo = DbsCommonPart(DatastoreMongo,private);		
-
-module.exports = DatastoreMongo;
